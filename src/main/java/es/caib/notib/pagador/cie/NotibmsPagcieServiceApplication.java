68cfe9ddc7f5b0657a23ada725907b87a285b403
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/pagador/cie/NotibmsPagcieServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.pagador.cie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsPagcieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsPagcieServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/pagador/cie/NotibmsPagcieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.pagador.cie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsPagcieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
